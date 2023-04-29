bc190ef8f356fffcceb01b7d59175d90d11bfba5
Patel Yash <45962177+yash8005@users.noreply.github.com>
2023-04-29 18:44:52 -0500

    Added model selector

--- main.py
+++ main.py
@@ -8,15 +8,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from streamlit_chat import message
 import streamlit as st
 
-
-
-template = """
-
-    QUERY: {query}
-    CONTEXT: {context}
-    
-"""
-
 def load_T5():
     llm_t5 = HuggingFaceHub(repo_id="google/flan-t5-small", model_kwargs={"temperature":0, "max_length":64})
     return llm_t5
@@ -43,8 +34,6 @@ with left_column:
 
 context_input = get_text()
 
-
-
 with right_column:
     st.subheader("Please Enter the Query:")
     input_query = st.text_input(label="Query Input",label_visibility='collapsed', placeholder="Query...", key="query_input")
@@ -54,36 +43,49 @@ with right_column:
     query_text = get_query()
 
     def get_t5_output(texts):
-        embeddings = HuggingFaceEmbeddings()
-        docsearch = Chroma.from_texts(texts, embeddings)
-        qa = RetrievalQA.from_chain_type(llm=llm_t5, chain_type="stuff", retriever=docsearch.as_retriever(search_kwargs={"k": 1}),input_key="question")
-        output_t5 = qa.run(query_text)
-        return output_t5
+        with st.spinner(f"Running T5..."):
+            embeddings = HuggingFaceEmbeddings()
+            docsearch = Chroma.from_texts(texts, embeddings)
+            qa = RetrievalQA.from_chain_type(llm=llm_t5, chain_type="stuff", retriever=docsearch.as_retriever(search_kwargs={"k": 1}),input_key="question")
+            output_t5 = qa.run(query_text)
+            return output_t5
     
     def get_openai_output(texts):
-        openai_embeddings = OpenAIEmbeddings()
-        openai_docsearch = Chroma.from_texts(texts, openai_embeddings)
-        qa_openai = RetrievalQA.from_chain_type(llm=OpenAI(temperature=0), chain_type="stuff", retriever=openai_docsearch.as_retriever(search_kwargs={"k": 1}),input_key="question")
-        output_openai = qa_openai.run(query_text)
-        return output_openai
+        with st.spinner(f"Running OpenAI..."):
+            openai_embeddings = OpenAIEmbeddings()
+            openai_docsearch = Chroma.from_texts(texts, openai_embeddings)
+            qa_openai = RetrievalQA.from_chain_type(llm=OpenAI(temperature=0), chain_type="stuff", retriever=openai_docsearch.as_retriever(search_kwargs={"k": 1}),input_key="question")
+            output_openai = qa_openai.run(query_text)
+            return output_openai
+           
 
+    st.markdown("### Output:")
+    t5_selected = st.checkbox("Google Flan-T5", value=True)
+    openai_selected = st.checkbox("OpenAI text-davinci-003", value=True)
+    get_answer_button = st.button("Click for answer", type='secondary')
+    t5_col,openai_col = st.columns([5,5])
+    with t5_col:
+        st.subheader('_Output from :red[T5]:_')
 
-    def get_answer():
+    with openai_col:
+        st.subheader('_Output from :red[OpenAI]:_ ')
+
+    if get_answer_button:
+        if not(openai_selected or t5_selected):
+            st.error('Please select atleaset 1 Model.', icon="🚨")
+
         if context_input and query_text:
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=20000, chunk_overlap=200,length_function = len)
             texts = text_splitter.split_text(context_input)
-            output_t5 = get_t5_output(texts)
-            output_openai = get_openai_output(texts)
-            t5_col,openai_col = st.columns([5,5])
             with t5_col:
-                st.write('Output from T5:')
-                st.write(output_t5)
+                if(t5_selected):
+                    output_t5 = get_t5_output(texts)
+                    st.write(output_t5)
             with openai_col:
-                st.write('Output from T5:')
-                st.write(output_openai)
-
-    st.markdown("### Output:")
-    st.button("Click for answer", type='secondary', on_click=get_answer())
+                if(openai_selected):
+                    output_openai = get_openai_output(texts)
+                    st.write(output_openai)
+        
 
 
 
